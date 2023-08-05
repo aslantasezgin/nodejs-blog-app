25eef677082eb2ac4bda2375b91d18f53c5faaa2
aslantasezgin <aslantasezginn@gmail.com>
2023-08-06 01:55:17 +0300

    middleware

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const express = require("express")
 const blogApp = express()
 
 
+blogApp.use(function(request, response, next){
+    console.log("middleware1")
+    next()
+})
+
+blogApp.use(function(request, response, next){
+    next()
+
+})
+
 blogApp.use(function(request, response){
     
-    response.end("homepagee")
-})
+    response.send("<h1>homepagex </h1>")
+ })
+ 
 
 blogApp.listen(3000, function () {
     console.log("port 3000")

8ab2c30f531596273bbf8f0fa337f1f47d1c9e4f
aslantasezgin <aslantasezginn@gmail.com>
2023-08-06 02:08:35 +0300

    views and path.join

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const express = require("express")
 const blogApp = express()
 
 
+const path = require("path")
+
+
 blogApp.use("/blogs/:id",function(request, response, next){
-    console.log(request.params)
-    response.send("Blog Detay Sayfası" + " " + request.params.id)
+
+response.sendFile(path.join(__dirname, "views/users", "blog-details.html"))
 })
 
 blogApp.use("/blogs",function(request, response, next){
-    response.send("Blogs")
+    response.sendFile(path.join(__dirname, "views/users", "blogs.html"))
+
 })
 
 blogApp.use("/",function(request, response, next){
-    response.send("Anasayfa")
+    response.sendFile(path.join(__dirname, "views/users", "index.html"))
+
 })
 
 
-
-
-
- 
 
 blogApp.listen(3000, function () {
     console.log("port 3000")

9e5dbaf8f7fc7fb6b93eda3256586602826fc378
aslantasezgin <aslantasezginn@gmail.com>
2023-08-13 03:12:14 +0300

    partial views

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,34 @@ const path = require("path")
 
 const data ={
 title:"Popüler Kurslar",
-categories:["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"] 
+categories:["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
+blogs:[
+{
+id:1,
+blogTitle:"Web Geliştirme 101",
+blogDescription:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, debitis.",
+blogImg:"1.jpeg",
+isFeatured:true,
+isAgree:true,
+},
+{
+id:2,
+blogTitle:"Web Geliştirme 201",
+blogDescription:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, debitis.",
+blogImg:"2.jpeg",
+isFeatured:true,
+isAgree:false,
+
+},
+{
+id:3,
+blogTitle:"Web Geliştirme 301",
+blogDescription:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, debitis.",
+blogImg:"3.jpeg",
+isFeatured:false,
+isAgree:true,
+},
+]
 }
 
 router.use("/blogs/:id",function(request, response){
@@ -13,7 +40,7 @@ response.render("users/blog-details")
 })
     
 router.use("/blogs",function(request, response){
-response.render("users/blogs")
+response.render("users/blogs", data)
 })
     
 router.use("/",function(request, response){
